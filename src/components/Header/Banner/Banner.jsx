de6d634c67e5b5eced717e239504421fb16ed223
husneara <husnearaa99@gmail.com>
2023-09-26 22:31:15 +0600

    updated banner section

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -1,20 +1,30 @@
 
 const Banner = () => {
     return (
-        <div className="h-[70vh] bg-slate-300">
-           <div className="text-center items-center">
-           <div className="max-w-md">
-           <h1 className="mb-5 pt-10 text-4xl font-bold">I Grow By Helping People In Need</h1>
-            <div className="relative gap-2 md:w-max text-center items-center">
-               <input type="text"className="input w-24 md:w-auto rounded-lg" placeholder="Search here "/>
-                <button  className=" !absolute right-1 top-1 py-2 px-6 bg-[#FF444A] text-white rounded">
-                    Search
-                </button>
+        <div className="h-[70vh] relative">
+            <div className="">
+                <img className="h-[70vh] w-full max-w-[1300px] mx-auto absolute " src={"https://i.ibb.co/Q6j07CL/Rectangle-4281.png"} alt="" />
+                <div className="absolute inset-0 bg-white opacity-90"></div>
+
+                <div className="relative flex justify-center items-center pt-40">
+                    <h1 className="text-4xl text-black  absolute font-bold">I Grow By Helping People In Need</h1>
+                </div>
+                <div className="flex justify-center items-center">
+                    <input type="text" className="input relative rounded-sm mt-10 w-1/5 border-[#DEDEDE] " placeholder="Search here " />
+                   
+                    <button
+                        className=" absolute  bg-[#FF444A] py-4 mt-10 px-6 ml-48 mr-5 rounded-sm text-xs font-bold  text-white "
+                        type="button"
+                      
+                    >
+                        Search
+                    </button>
+                </div>
             </div>
-           </div>
-           </div>
+
         </div>
     );
 };
 
 export default Banner;
+
